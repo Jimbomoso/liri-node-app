3b08e850c1ffca0aa9694b85f92cbfac16b12ca9
Jimbomoso <jimbomoso@gmail.com>
2019-06-03 15:21:52 -0400

    added switch statement

--- liri.js
+++ liri.js
@@ -10,3 +10,21 @@ var spotify = new Spotify(keys.spotify);
 var request = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
 
+switch (request) {
+    case "concert-this":
+        bitThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+
+    case "movie-this":
+        ombdThis();
+        break;
+
+    case "do-what-it-says":
+        doIt();
+        break;
+}
+
