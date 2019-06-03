376ebdb4f7de26abc57bdb07f47afa18d98f7b64
Jimbomoso <jimbomoso@gmail.com>
2019-06-03 15:15:17 -0400

    added variables

--- liri.js
+++ liri.js
@@ -1,14 +1,12 @@
 // read and set environment variables with dotenv package
 require("dotenv").config();
-
-// import file structure
 var fs = require("fs");
-// import keys file
+var moment = require("moment");
 var keys = require("./keys.js");
-// import request 
 var axios = require("axios");
-
-// Spotify 
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+var request = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
 
