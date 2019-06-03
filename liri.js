a177e9c95e97d74c9331d8af214383f41b1a3b57
Jimbomoso <jimbomoso@gmail.com>
2019-06-03 12:13:00 -0400

    imports created

--- liri.js
+++ liri.js
@@ -1,6 +1,14 @@
 // read and set environment variables with dotenv package
 require("dotenv").config();
 
+// import file structure
+var fs = require("fs");
 // import keys file
 var keys = require("./keys.js");
+// import request 
+var axios = require("axios");
+
+// Spotify 
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
